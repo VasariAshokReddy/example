6008332f6441a23ca546e1c16567448e850f50d5
VasariAshokReddy <ashokreddy.va5@gmail.com>
2018-10-31 13:24:40 +0530

    Test code changed

--- Basics/src/com/app/Test.java
+++ Basics/src/com/app/Test.java
@@ -5,9 +5,17 @@ public class Test {
 	static int a=10;
 	static int b=10;
 	static int c =a+b;
+	
+	public void m()
+	{
+		System.out.println("=======");
+	}
 	public static void main(String[] args) {
 
 		System.out.println("Value of c "+Test.c);
+		
+		Test t = new Test();
+		t.m();
 	}
 
 }
