d09e3b91fbe918e11c3a1ac5ae29e45ea1522598
VasariAshokReddy <ashokreddy.va5@gmail.com>
2018-10-31 13:14:31 +0530

    code changed

--- Basics/src/com/app/Sample.java
+++ Basics/src/com/app/Sample.java
@@ -4,6 +4,7 @@ public class Sample {
 
 	static int  i=10;
 	
+	static String name = "ashok";
 	public void m()
 	{
 		System.out.println("method");
@@ -11,6 +12,7 @@ public class Sample {
 	public static void main(String[] args) {
 
 		System.out.println("Static value "+Sample.i);
+		System.out.println("String name "+Sample.name);
 		Sample s= new Sample();
 		s.m();
 	}
