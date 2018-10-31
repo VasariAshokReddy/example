67fec848c54f8904fe6c29d49fdc9b8ddae708ac
VasariAshokReddy <ashokreddy.va5@gmail.com>
2018-10-31 13:20:04 +0530

    new program

---
+++ Basics/src/com/app/Test.java
@@ -0,0 +1,13 @@
+package com.app;
+
+public class Test {
+
+	static int a=10;
+	static int b=10;
+	static int c =a+b;
+	public static void main(String[] args) {
+
+		System.out.println("Value of c "+Test.c);
+	}
+
+}
