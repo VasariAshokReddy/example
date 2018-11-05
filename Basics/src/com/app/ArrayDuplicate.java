c8d18cf8b806e6c9e8c0bf52c9bde39a1a094ff4
VasariAshokReddy <ashokreddy.va5@gmail.com>
2018-11-05 11:29:25 +0530

    duplicate array

---
+++ Basics/src/com/app/ArrayDuplicate.java
@@ -0,0 +1,33 @@
+package com.app;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArrayDuplicate {
+
+	 static void duplicateArray(int array[])
+	{
+		
+		Map<Integer,Integer> map  = new HashMap<Integer, Integer>();
+		for(int arr:array)
+		{
+			if(map.containsKey(arr))
+			{
+				map.put(arr, map.get(arr)+1);
+			}
+			else
+			{
+				map.put(arr, 1);
+			}
+		}
+		System.out.println("Input array"+Arrays.toString(array));
+		System.out.println("Element count "+map);
+	}
+	
+	public static void main(String[] args) {
+
+		duplicateArray(new int[] {2,5,3,2,4,3,5});
+	}
+
+}
