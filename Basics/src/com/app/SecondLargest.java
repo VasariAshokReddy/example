19b705f52c9792a322039fa5868cd2a2848b70ee
VasariAshokReddy <ashokreddy.va5@gmail.com>
2018-11-05 11:58:22 +0530

    added

--- Basics/src/com/app/SecondLargest.java
+++ Basics/src/com/app/SecondLargest.java
@@ -13,7 +13,9 @@ public class SecondLargest {
 	public static void main(String[] args) {
 
 		int a[] = {54,65,2,87,95,4};
+		int a2[] = {54,65,12,78,96,4};
 		System.out.println("Second Largest :"+secondLar(a,6));
+		System.out.println("Second Largest :"+secondLar(a2,6));
 	}
 
 }
