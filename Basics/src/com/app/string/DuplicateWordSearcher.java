6cf5cc1b46fcf636089cf921c4a855352009742e
VasariAshokReddy <ashokreddy.va5@gmail.com>
2018-11-05 23:34:02 +0530

    duplicate word serach

---
+++ Basics/src/com/app/string/DuplicateWordSearcher.java
@@ -0,0 +1,24 @@
+package com.app.string;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class DuplicateWordSearcher {
+
+	public static void main(String[] args) {
+
+		String text = "a r b k c d se f g a d f s s f d s ft gh f ws w f v x s g h d h j j k f sd j e wed a d f";
+		 
+        List<String> list = Arrays.asList(text.split(" "));
+ 
+        Set<String> uniqueWords = new HashSet<String>(list);
+        for (String word : uniqueWords) {
+            System.out.print(word + ": " + Collections.frequency(list, word));
+        }
+	
+	}
+
+}
