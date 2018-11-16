ce2bd2b2f888cfe4c6186ee5b68c4d4fa8ddbd8b
VasariAshokReddy <ashokreddy.va5@gmail.com>
2018-11-16 19:18:03 +0530

    value tag add

--- SpringPractice/src/main/java/org/nt/Main.java
+++ SpringPractice/src/main/java/org/nt/Main.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 
 	@SuppressWarnings("resource")
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception,Exception{
 
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("abc.xml");
 		Employee emp = (Employee)ctx.getBean("emp");
